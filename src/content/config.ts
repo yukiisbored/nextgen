d5a8d4c6510fde3b94053a28cc82b0db9bce37c6
Yuki Langley <hi@yukiisbo.red>
2026-02-26 10:28:56 +0100

    Exclude posts from feed

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
 const notesCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		date: z.string(),
-		description: z.string().optional(),
-		draft: z.boolean().optional(),
-		slug: z.string().optional(),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    description: z.string().optional(),
+    draft: z.boolean().optional(),
+    excludeFeed: z.boolean().optional(),
+    slug: z.string().optional(),
+  }),
 });
 
 export const collections = {
-	notes: notesCollection,
+  notes: notesCollection,
 };
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,31 +7,31 @@ import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import invariant from "tiny-invariant";
 
 export async function GET(context: APIContext) {
-	const renderers = await loadRenderers([getMDXRenderer()]);
-	const container = await AstroContainer.create({ renderers });
-	const posts = (await getCollection("notes")).filter(
-		(post) => !post.data.draft,
-	);
+  const renderers = await loadRenderers([getMDXRenderer()]);
+  const container = await AstroContainer.create({ renderers });
+  const posts = (await getCollection("notes")).filter(
+    (post) => !post.data.draft && !post.data.excludeFeed,
+  );
 
-	const items: RSSFeedItem[] = [];
-	for (const post of posts) {
-		const { Content } = await render(post);
-		const content = await container.renderToString(Content);
-		items.push({
-			title: post.data.title,
-			pubDate: new Date(post.data.date),
-			description: post.data.description,
-			link: `/notes/${post.slug}/`,
-			content,
-		});
-	}
+  const items: RSSFeedItem[] = [];
+  for (const post of posts) {
+    const { Content } = await render(post);
+    const content = await container.renderToString(Content);
+    items.push({
+      title: post.data.title,
+      pubDate: new Date(post.data.date),
+      description: post.data.description,
+      link: `/notes/${post.slug}/`,
+      content,
+    });
+  }
 
-	invariant(context.site, "site is required");
-	return rss({
-		title: "Yuki's Notes",
-		description: "The writings of a computer hobbyist dork",
-		site: context.site,
-		items,
-		stylesheet: "/rss/styles.xsl",
-	});
+  invariant(context.site, "site is required");
+  return rss({
+    title: "Yuki's Notes",
+    description: "The writings of a computer hobbyist dork",
+    site: context.site,
+    items,
+    stylesheet: "/rss/styles.xsl",
+  });
 }
