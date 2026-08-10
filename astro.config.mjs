8a1978be83290ba3f6c54c0a262aec124e00d004
Yuki Langley <hi@yukiisbo.red>
2026-08-10 03:22:56 +0200

    I'm dumb as hell :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import { unified } from "@astrojs/markdown-remark";
 import rehypeOGCard from "rehype-og-card";
 
 // @ts-expect-error Too lazy to fix tsconfig.json
-const ci = process.env.CI === undefined;
+const ci = process.env.CI !== undefined;
 
 // https://astro.build/config
 export default defineConfig({
@@ -29,7 +29,7 @@ export default defineConfig({
           // See https://github.com/Robot-Inventor/astro-link-card/blob/main/src/index.ts#L16
           {
             buildCache: !ci,
-            buildCachePath: "./node_modules/.astro",
+            buildCachePath: "./.cache/og-card",
             enableSameTextURLConversion: true,
             serverCache: true,
             serverCachePath: "./public",
